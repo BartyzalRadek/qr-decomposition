e690fa27bf72a782c5dee6c3bf9d1d699c8294f7
Radek Bartyzal <rbartyzal1@gmail.com>
2017-05-07 18:59:00 +0200

    Fixed Gk generation

--- qr.py
+++ qr.py
@@ -4,15 +4,21 @@ A = np.matrix([[6, 5, 0],
                [5, 1, 4],
                [0, 4, 3]], dtype=float)
 A2 = np.matrix([[6, 5, 0],
-               [5, 1, 4],
-               [0, 4, 3]], dtype=float)
+                [5, 1, 4],
+                [0, 4, 3]], dtype=float)
+A3 = np.matrix([[6, 5, 0],
+                [5, 1, 4],
+                [3, 2, 1],
+                [0, 4, 3]], dtype=float)
 
 pos = [0, 1]
-print(A[pos[0], pos[1]])
+print('A=\n', A)
+
 
 def replace_elems_close_to_zero(A):
     EPSILON = 0.000000001
     A[np.abs(A) < EPSILON] = 0
+
 
 def get_rotation_matrix(A, pos_to_zero):
     """
@@ -25,42 +31,49 @@ def get_rotation_matrix(A, pos_to_zero):
     j = pos_to_zero[1]
 
     G = np.identity(A.shape[0], dtype=float)
-    # [a, b] = vector, b will be zeroed out by matrix:
+    # [a, b] = vector, b will be zeroed out by matrix, a = A[j,j]
     #
-    # |c -s| |a| = |r|
-    # |s  c| |b| = |0|
-    a = A[i - 1, j]
+    # |alpha -beta| |a| = |r|
+    # |beta  alpha| |b| = |0|
+    a = A[j, j]
     b = A[i, j]
+    alpha = 0
+    beta = 0
+    if (np.abs(b) >= np.abs(a)):
+        t = -a / b
+        beta = 1 / np.sqrt(1 + t * t)
+        alpha = beta * t
+    else:
+        t = -b / a
+        alpha = 1 / np.sqrt(1 + t * t)
+        beta = alpha * t
 
-    r = np.sqrt(a * a + b * b)
-    c = a / r
-    s = -b / r
-
-    G[i - 1, j] = c
-    G[i - 1, j + 1] = -s
-    G[i, j] = s
-    G[i, j + 1] = c
+    G[i, i] = alpha
+    G[j, j] = alpha
+    G[i, j] = beta
+    G[j, i] = -beta
 
     return np.matrix(G)
 
 
-G1 = get_rotation_matrix(A, [1,0])
-print('G1 for [1,0]:\n',G1)
-A= G1*A
+G1 = get_rotation_matrix(A, [1, 0])
+print('G1 for [1,0]:\n', G1)
+A = G1 * A
 replace_elems_close_to_zero(A)
-print('G1*A for [1,0]:\n',A)
+print('G1*A for [1,0]:\n', A)
 
-G2 = get_rotation_matrix(A, [2,1])
-print('G2 for [2,1]:\n',G2)
-A= G2*A
+G2 = get_rotation_matrix(A, [2, 1])
+print('G2 for [2,1]:\n', G2)
+A = G2 * A
 replace_elems_close_to_zero(A)
-print('G2*A for [2,1]:\n',A)
+print('G2*A for [2,1]:\n', A)
 
-R=A
-QT = G1.T*G2.T
+R = A
+QT = G1.T * G2.T
 print('Q.T:\n', QT)
 
-print('Q * A = R\nA = Q.T * R\n', QT*R)
+print('Q * A = R\nA = Q.T * R\n', QT * R)
+
 
 def QR_decomposition(A):
     """
@@ -79,18 +92,18 @@ def QR_decomposition(A):
         for i in range(A.shape[0]):
             for j in range(A.shape[1]):
                 if j >= i:
-                    #iterate only under diagonal
+                    # iterate only under diagonal
                     break
 
-                if R[i,j] != 0:
-                    print('Zeroing elem at ', [i,j])
-                    G = get_rotation_matrix(R, [i,j])
-                    #print('G to zero elem at {} = \n{}'.format([i,j], G))
-                    R = G*R
+                if R[i, j] != 0:
+                    print('Zeroing elem at ', [i, j])
+                    G = get_rotation_matrix(R, [i, j])
+                    # print('G to zero elem at {} = \n{}'.format([i,j], G))
+                    R = G * R
                     replace_elems_close_to_zero(R)
-                    #print('G*R = \n', R)
-                    QT = QT*G.T
-                    #print('QT*G.T = \n', QT)
+                    # print('G*R = \n', R)
+                    QT = QT * G.T
+                    # print('QT*G.T = \n', QT)
                     restart = True
                     break
 
@@ -98,8 +111,9 @@ def QR_decomposition(A):
 
     replace_elems_close_to_zero(QT)
     Q = QT.T
-    print('Q: \n{}\nR: \n{} \nQ * A = R\nA = Q.T * R = \n{}'.format(Q, R, QT*R))
+    print('Q: \n{}\nR: \n{} \nQ * A = R\nA = Q.T * R = \n{}'.format(Q, R, QT * R))
     return Q, R
+
 
 print('*****************')
 QR_decomposition(A2)
