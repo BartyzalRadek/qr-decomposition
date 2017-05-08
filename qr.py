c15448d23334fe5ca476143e9efd2e39d72b1144
Radek Bartyzal <rbartyzal1@gmail.com>
2017-05-08 15:35:17 +0200

    Decomposition using precalculated QR from smaller matrix.

--- qr.py
+++ qr.py
@@ -10,6 +10,8 @@ A3 = np.matrix([[6, 5, 0],
                 [5, 1, 4],
                 [3, 2, 1],
                 [0, 4, 3]], dtype=float)
+A4 = np.matrix([[6, 5, 0],
+                [0, 4, 3]], dtype=float)
 
 pos = [0, 1]
 print('A=\n', A)
@@ -18,6 +20,7 @@ print('A=\n', A)
 def replace_elems_close_to_zero(A):
     EPSILON = 0.000000001
     A[np.abs(A) < EPSILON] = 0
+    return A
 
 
 def get_rotation_matrix(A, pos_to_zero):
@@ -78,36 +81,27 @@ print('Q * A = R\nA = Q.T * R\n', QT * R)
 def QR_decomposition(A):
     """
     QR decomposition of matrix A using Givens rotations.
-    https://en.wikipedia.org/wiki/Givens_rotation
     :param A: Matrix to decompose.
     :return: Q,R
     """
     print('QR decomposition for matrix A: \n', A)
     QT = np.matrix(np.identity(A.shape[0]))
     R = np.matrix(A)
-    restart = True
-    while restart:
-        restart = False
-        # If A is not triangular - restart search for another non-zero element
-        for i in range(A.shape[0]):
-            for j in range(A.shape[1]):
-                if j >= i:
-                    # iterate only under diagonal
-                    break
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            if j >= i:
+                # iterate only under diagonal
+                break
 
-                if R[i, j] != 0:
-                    print('Zeroing elem at ', [i, j])
-                    G = get_rotation_matrix(R, [i, j])
-                    # print('G to zero elem at {} = \n{}'.format([i,j], G))
-                    R = G * R
-                    replace_elems_close_to_zero(R)
-                    # print('G*R = \n', R)
-                    QT = QT * G.T
-                    # print('QT*G.T = \n', QT)
-                    restart = True
-                    break
-
-            if restart: break
+            if R[i, j] != 0:
+                print('Zeroing elem at ', [i, j])
+                G = get_rotation_matrix(R, [i, j])
+                # print('G to zero elem at {} = \n{}'.format([i,j], G))
+                R = G * R
+                replace_elems_close_to_zero(R)
+                # print('G*R = \n', R)
+                QT = QT * G.T
+                # print('QT*G.T = \n', QT)
 
     replace_elems_close_to_zero(QT)
     Q = QT.T
@@ -115,5 +109,61 @@ def QR_decomposition(A):
     return Q, R
 
 
+def OLS_from_QR(R, b):
+    w = np.linalg.solve(a=R, b=b)
+    return w
+
+
+def QR_decomposition2(old_Q, old_R, A2):
+    """
+    QR decomposition of matrix A using Givens rotations.
+    :param old_Q: Q calculated for matrix A1
+    :param old_R: R calculated for matrix A1
+    :param A2: New additional rows to original matrix A1
+    :return: QR decompositioin of matrix A = concatenated A1 with A2 using precalculated Q, R from A1
+    """
+    A1 = replace_elems_close_to_zero(old_Q.T * old_R)  # original matrix A1
+
+    if A1.shape[1] != A2.shape[1]:
+        print('Provided matrix has different number of columns ({}) than the original one ({}).\n'
+              'Returning...'.format(A2.shape[1], A1.shape[1]))
+        return None
+
+    A = np.concatenate((A1, A2), axis=0)  # Add all rows from A2 to A1 = create whole matrix A
+    QT = np.matrix(np.identity(A.shape[0]))
+
+    for i in range(old_Q.shape[0]):
+        for j in range(old_Q.shape[1]):
+            QT[i, j] = old_Q.T[i, j]
+
+
+    # R = replace_elems_close_to_zero(QT.T * A) # equal to: np.concatenate((old_R, A2), axis=0)
+    R = np.concatenate((old_R, A2), axis=0)
+    print('Q*A = R:\n', R)
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            if j >= i:
+                # iterate only under diagonal
+                break
+
+            if R[i, j] != 0:
+                print('Zeroing elem at ', [i, j])
+                G = get_rotation_matrix(R, [i, j])
+                # print('G to zero elem at {} = \n{}'.format([i,j], G))
+                R = G * R
+                replace_elems_close_to_zero(R)
+                # print('G*R = \n', R)
+                QT = QT * G.T
+                # print('QT*G.T = \n', QT)
+
+    replace_elems_close_to_zero(QT)
+    Q = QT.T
+    print('Q: \n{}\nR: \n{} \nQ * A = R\nA = Q.T * R = \n{}'.format(Q, R, replace_elems_close_to_zero(QT * R)))
+    return Q, R
+
+
+
 print('*****************')
-QR_decomposition(A2)
+Q, R = QR_decomposition(A3)
+print('*****************')
+QR_decomposition2(old_Q=Q, old_R=R, A2=A4)
